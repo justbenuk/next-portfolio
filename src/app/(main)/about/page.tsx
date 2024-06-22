2ce6d60a85c9b5a5b4bcfbccbc382f7a557e45b1
justbenuk <justbenuk@gmail.com>
2024-06-22 14:15:20 +0100

    initial commit

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -3,9 +3,9 @@ import Quote from "@/components/ui/quote";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'My Story So Far | Just Ben UK',
-  description: "I am a self taught developer building web applicatiuons for fun"
-}
+  title: "My Story So Far | Just Ben UK",
+  description: "I am a self taught developer building web applications for fun",
+};
 export default function AboutPage() {
   return (
     <div className="max-w-7xl mx-auto px-6 xl:px-0 py-36">
@@ -13,32 +13,70 @@ export default function AboutPage() {
       <div className="grid grid-cols-1 lg:grid-cols-2 mt-8 gap-10">
         <div>
           <div>
-            <h1 className="ytitle uppercase text-xs font-bold">Where Did It Start?</h1>
-            <p>Many moons ago I started on a forum here in the uk. I was just a young lad that enjoyed talking to people from across the globe. After a year or two I was aksed if I would help out and become a moderator, I started working on the some of the code in the background making changes to the look and feel of the forum. Thats when I found the bug for coding.</p>
+            <h1 className="ytitle uppercase text-xs font-bold">
+              Where Did It Start?
+            </h1>
+            <p>
+              Many moons ago I started on a forum here in the uk. I was just a
+              young lad that enjoyed talking to people from across the globe.
+              After a year or two I was asked if I would help out and become a
+              moderator, I started working on the some of the code in the
+              background making changes to the look and feel of the forum. Thats
+              when I found the bug for coding.
+            </p>
           </div>
           <div className="py-8">
-            <h1 className="ytitle uppercase text-xs font-bold">From PHP to WordPress</h1>
-            <p>I started to look at other things I could do, I built a few workpress websites for friends mainly using free themes from the internet, but I always felt I could acheive more it I started to learn more. So I started building custom themes and plugins for wordpress.</p>
+            <h1 className="ytitle uppercase text-xs font-bold">
+              From PHP to WordPress
+            </h1>
+            <p>
+              I started to look at other things I could do, I built a few
+              wordpress websites for friends mainly using free themes from the
+              internet, but I always felt I could achieve more it I started to
+              learn more. So I started building custom themes and plugins for
+              wordpress.
+            </p>
           </div>
         </div>
         <div>
           <div>
-            <h1 className="ytitle uppercase text-xs font-bold">And now what?</h1>
-            <p>Overtime I started to move on to more bigger projects that WordPress would not be able to do with out a lot of work. I thought it would be so much easier if I could just build my own web application. I just so happened to stumbnle across a video about React and NextJS, then it hit me. I want to be able to build fullstack applications with NextJS</p>
+            <h1 className="ytitle uppercase text-xs font-bold">
+              And now what?
+            </h1>
+            <p>
+              Overtime I started to move on to more bigger projects that
+              WordPress would not be able to do with out a lot of work. I
+              thought it would be so much easier if I could just build my own
+              web application. I just so happened to stumble across a video
+              about React and NextJS, then it hit me. I want to be able to build
+              fullstack applications with NextJS
+            </p>
           </div>
           <div className="py-8">
-            <h1 className="ytitle uppercase text-xs font-bold">And Here we are now</h1>
-            <p>I am 40 years old, with a full time job, but my passion is learning everything I can. I spend all my freetime building web applications, but i've never really build a portfolio to show them. <span className="ytitle font-bold">Well Now I Have</span>. Im always looking for feed back. So please take a look at some of my projects and let me know what you think</p>
+            <h1 className="ytitle uppercase text-xs font-bold">
+              And Here we are now
+            </h1>
+            <p>
+              I am 40 years old, with a full time job, but my passion is
+              learning everything I can. I spend all my free time building web
+              applications, but i've never really build a portfolio to show
+              them. <span className="ytitle font-bold">Well Now I Have</span>.
+              Im always looking for feed back. So please take a look at some of
+              my projects and let me know what you think
+            </p>
           </div>
         </div>
       </div>
       <div className="flex flex-row justify-end">
         <div className="mt-6">
-          <Link href='/projects' className="border rounded-xl border-[#f4d956] px-8 py-2 hover:bg-[#f4d956] hover:text-black">My Projects</Link>
+          <Link
+            href="/projects"
+            className="border rounded-xl border-[#f4d956] px-8 py-2 hover:bg-[#f4d956] hover:text-black"
+          >
+            My Projects
+          </Link>
         </div>
       </div>
     </div>
-
-
   );
 }
--- src/app/(main)/services/page.tsx
+++ src/app/(main)/services/page.tsx
@@ -1,21 +1,34 @@
-import Quote from "@/components/ui/quote"
-import ServiceItem from "@/components/services/service-item"
-import type { Metadata } from "next"
+import Quote from "@/components/ui/quote";
+import ServiceItem from "@/components/services/service-item";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'What I Can Do | Just Ben UK',
-  description: "I am always trying to better myself, by creating more and more complex web applications. This is what I can do at the moment"
-}
+  title: "What I Can Do | Just Ben UK",
+  description:
+    "I am always trying to better myself, by creating more and more complex web applications. This is what I can do at the moment",
+};
 export default function ServicesPage() {
   return (
     <div className="max-w-7xl mx-auto px-6 xl:px-0">
-      <Quote text="I never claim to be anything diffrent, I am self-taught and love to build things" />
+      <Quote text="I never claim to be anything different, I am self-taught and love to build things" />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mt-10">
-        <ServiceItem title='Simple Blogs' description='A fullstack blog application that can be used to display information about your services or products that doesnt require an online strore' />
-        <ServiceItem title='SPA (Single Page Application)' description='These are perfect for single page websites that would like to show their brand. Requires just a few pages to show location, contact details and information about the services you offer' />
-        <ServiceItem title='Get Data from Another Source' description="Do you need to display information from another source. I work with API's too. This can be used for internal or externals use." />
-        <ServiceItem title='Online Store' description="This is what I am working towards now, Building fully managed online stores customised to your requirements, incluiding carts, payments and much much more" />
+        <ServiceItem
+          title="Simple Blogs"
+          description="A fullstack blog application that can be used to display information about your services or products that doesn't require an online store"
+        />
+        <ServiceItem
+          title="SPA (Single Page Application)"
+          description="These are perfect for single page websites that would like to show their brand. Requires just a few pages to show location, contact details and information about the services you offer"
+        />
+        <ServiceItem
+          title="Get Data from Another Source"
+          description="Do you need to display information from another source. I work with API's too. This can be used for internal or externals use."
+        />
+        <ServiceItem
+          title="Online Store"
+          description="This is what I am working towards now, Building fully managed online stores customised to your requirements, including carts, payments and much much more"
+        />
       </div>
     </div>
-  )
+  );
 }
